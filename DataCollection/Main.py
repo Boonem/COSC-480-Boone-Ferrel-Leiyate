1984fdcd960ca15298e2925d14b5e724582c84f3
Boonem <mcboone228@gmail.com>
2024-09-29 13:42:34 -0400

    Testing Spotipy Creds. with Example

--- DataCollection/Main.py
+++ DataCollection/Main.py
@@ -1,5 +1,25 @@
-#Import spotipy library
 import spotipy
-from spotipy.oath2 import SpotifyClientCredentials
+from spotipy.oauth2 import SpotifyClientCredentials
+import os
+from dotenv import load_dotenv
 
-import Config
+#Get API Ids from .env
+load_dotenv()
+
+cid = os.getenv('client_id')
+secret = os.getenv('client_secret')
+
+#Apply API Ids for Spotipy
+client_credentials_manager = SpotifyClientCredentials(client_id=cid, client_secret=secret)
+sp = spotipy.Spotify(client_credentials_manager = client_credentials_manager)
+
+#Example data grab
+steely_uri = 'spotify:artist:6P7H3ai06vU1sGvdpBwDmE'
+results = sp.artist_albums(steely_uri, album_type='album')
+albums = results['items']
+while results['next']:
+    results = sp.next(results)
+    albums.extend(results['items'])
+
+for album in albums:
+    print(album['name'])
