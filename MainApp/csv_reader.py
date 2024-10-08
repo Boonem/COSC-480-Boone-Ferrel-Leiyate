bf9c6294280d67a01c6dd45e308d44e6cf81076d
drakeferrel <89860731+drakeferrel@users.noreply.github.com>
2024-10-07 21:44:12 -0400

    csv reader now saves objects, has test input file

--- MainApp/csv_reader.py
+++ MainApp/csv_reader.py
@@ -1,11 +1,15 @@
 import pandas as pd
 
 def read_csv(file_path):
+    output = []
     df = pd.read_csv(file_path)
-    for index, row in df.iterrows():
-        print(f"Row {index}:")
-        for column in df.columns:
-            print(f"Column '{column}': {row[column]}")
+    objects_list = [TestRecord(**row) for _, row in df.iterrows()]
+    print(vars(objects_list[0]))
+
+class TestRecord:
+    def __init__(self, **kwargs):
+        for key, value in kwargs.items():
+            setattr(self, key, value)
 
 file_path = 'input.csv'
 read_csv(input)
